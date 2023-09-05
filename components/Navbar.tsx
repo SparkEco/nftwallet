e131aba57ed13fd16c21a16b9cf72706f9f25ec5
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-09-05 19:47:40 +0100

    No time for comments XD

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="w-full flex lg:justify-center justify-around h-[60px] text-[14px] bg-neutral-800 items-center p-4">
+    <nav className="w-full flex lg:justify-center justify-around h-[60px] lg:text-[17px] text-[13px] bg-neutral-800 items-center p-4">
       <ul className="flex lg:space-x-[50px] space-x-[0.6rem] justify-center cursor-pointer">
         <li className="hover:text-sky-500">Home</li>
         <li className="hover:text-sky-500">Overview</li>
