811514e5c5ca29689ce918de508de81ecbceceec
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-09-05 19:34:06 +0100

    No time for comments XD

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,10 @@
 function Footer() {
   return (
-    <div className="w-full h-[280px] sticky bottom-1 bg-black  p-4 flex justify-around items-center">
-      <div className="rounded-[50%] border bg-gradient-to-tr from-red-700 to-orange-500 h-[100px] w-[100px] absolute left-3 top-[-40px]"></div>
-      <div className="h-[230px] rounded-[20px] w-[180px] bg-sky-600"></div>
-      <div className="h-[230px] rounded-[20px] w-[180px] bg-sky-600"></div>
-      <div className="h-[230px] rounded-[20px] w-[180px] bg-sky-600"></div>
+    <div className="w-full lg:h-[280px] h-[180px] sticky bottom-1 bg-black  p-4 flex justify-around items-center">
+      <div className="rounded-[50%] border bg-gradient-to-tr from-red-700 to-orange-500 h-[70px] w-[70px] lg:h-[100px] lg:w-[100px] absolute left-3 top-[-40px]"></div>
+      <div className="lg:h-[230px] h-[80%] rounded-[20px] lg:w-[180px] w-[30%] bg-sky-600"></div>
+      <div className="lg:h-[230px] h-[80%] rounded-[20px] lg:w-[180px] w-[30%] bg-sky-600"></div>
+      <div className="lg:h-[230px] h-[80%] rounded-[20px] lg:w-[180px] w-[30%] bg-sky-600"></div>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,12 @@
 function Navbar() {
   return (
     <nav className="w-full flex justify-center h-[60px] bg-neutral-800 items-center p-4">
-      <ul className="flex space-x-[50px] justify-center">
-        <li>Home</li>
-        <li>Overview</li>
-        <li>Marketplace</li>
-        <li>Collections</li>
-        <li>Metrics</li>
+      <ul className="flex lg:space-x-[50px] space-x-2 justify-center cursor-pointer">
+        <li className="hover:text-sky-500">Home</li>
+        <li className="hover:text-sky-500">Overview</li>
+        <li className="hover:text-sky-500">Marketplace</li>
+        <li className="hover:text-sky-500">Collections</li>
+        <li className="hover:text-sky-500">Metrics</li>
       </ul>
     </nav>
   );
