64dd054e1c972c37e51ab7ac676de4f9d2c6dec8
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-09-16 16:02:26 +0100

    NFT description tab in maps

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -7,6 +7,7 @@ import mapboxgl from "mapbox-gl";
 import geodata from "../../components/geolocation.json";
 import Image from "next/image";
 import { IoChevronBackSharp, IoChevronForwardSharp } from "react-icons/io5";
+import Share from "@/components/Share";
 
 function Main() {
   const ACCESS_TOKEN =
@@ -19,7 +20,7 @@ function Main() {
   );
   const [lat, setLat] = useState(39.8283);
   const [lng, setLng] = useState(-98.5795);
-  const [zoom, setZoom] = useState(2.5);
+  const [zoom, setZoom] = useState(3);
   const [curImage, setcurImg] = useState(0);
   const [imgs, setImgs] = useState<string[] | string>("");
 
@@ -79,22 +80,22 @@ function Main() {
       map.current?.remove();
     };
   }, []);
-  console.log(imgs);
 
   return (
-    <div className={`text-center relative`}>
+    <div className={`relative`}>
       <div ref={mapContainer} className="map-container h-[600px]" />
       {details != undefined ? (
         <div
-          className={`fixed block top-[80px] opacity-70 left-3 w-[350px] h-[80vh] bg-zinc-900/90 rounded-[30px] backdrop-blur py-4`}
+          className={`fixed block top-[80px] opacity-70 left-3 w-[400px] space-y-5 h-[80vh] bg-zinc-900/90 rounded-[10px] backdrop-blur p-4 overflow-y-auto text-white`}
         >
-          <p className={`text-white text-[18px] font-semibold`}>
+          <p className={`text-white text-center text-[18px] font-semibold`}>
             {details.name}
           </p>
           <div className="block w-[320px] relative h-[200px] mx-auto">
             <button
-              className={`absolute top-[40%] bg-[#80808080] left-4 rounded-[50%] hover:opacity-75 p-1`}
+              className={`absolute top-[40%] bg-zinc-900/40 disabled:bg-[#80808080] left-4 rounded-[50%] hover:opacity-75 p-1`}
               onClick={prevImg}
+              disabled={curImage == 0}
             >
               <IoChevronBackSharp size={26} color={`#ffffff`} />
             </button>
@@ -107,12 +108,70 @@ function Main() {
               className="block mx-auto rounded-[15px] lg:w-[320px] lg:h-[200px]"
             />
             <button
-              className={`absolute top-[40%] bg-[#80808080] right-4 rounded-[50%] hover:opacity-75 p-1`}
+              disabled={curImage == imgs.length - 1 ? true : false}
+              className={`absolute top-[40%] bg-zinc-900/40 disabled:bg-[#80808080] right-4 rounded-[50%] hover:opacity-75 p-1`}
               onClick={nextImg}
             >
               <IoChevronForwardSharp size={26} color={`#ffffff`} />
             </button>
           </div>
+          <Image
+            src={details.nftimg}
+            alt="NFT"
+            width={150}
+            height={150}
+            className={`block mx-auto rounded-[13px] w-[150px] h-[150px] border`}
+          />
+          <h1 className={`text-white text-[19px] font-semibold text-left`}>
+            Description
+          </h1>
+          <p className={`text-white text-[13px]`}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
+            deleniti eos nihil, dolores iusto excepturi nostrum exercitationem
+            mollitia, ut fugiat id! Corrupti laboriosam voluptates minima
+            eveniet quibusdam enim perferendis cum!
+          </p>
+          <h1 className={`text-center text-[19px] font-semibold`}>Owners</h1>
+          <div className={`flex justify-between px-3`}>
+            <p>Address</p>
+            <p>Share</p>
+          </div>
+          <Share
+            address="0x9c277ee7ef6974c73e51421419ff973579fd1d53"
+            percentage="22.40"
+          />
+          <Share
+            address="0x9c277ee7ef6974c73e51421419ff973579fd1d53"
+            percentage="0.73"
+          />
+          <Share
+            address="0x10cb0ab131f5bb879f7b91c86c9d48f971b6ba27"
+            percentage="1.44"
+          />
+          <Share
+            address="0x9c277ee7ef6974c73e51421419ff973579fd1d53"
+            percentage="2.32"
+          />
+          <Share
+            address="0x9c277ee7ef6974c73e51421419ff973579fd1d53"
+            percentage="11.00"
+          />
+          <Share
+            address="0x9c277ee7ef6974c73e51421419ff973579fd1d53"
+            percentage="7.01"
+          />
+          <Share
+            address="0x9c277ee7ef6974c73e51421419ff973579fd1d53"
+            percentage="4.33"
+          />
+          <Share
+            address="0x9c277ee7ef6974c73e51421419ff973579fd1d53"
+            percentage="5.08"
+          />
+          <Share
+            address="0x9c277ee7ef6974c73e51421419ff973579fd1d53"
+            percentage="2.99"
+          />
         </div>
       ) : null}
       {/* <StickyFooter /> */}
