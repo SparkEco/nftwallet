e567dedef6bb622157db0b355d6e22b71c615e26
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-09-23 19:02:41 +0100

    Added provider to app context

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -12,20 +12,21 @@ import Web3 from "web3";
 
 function Home() {
   const { provider } = useAppContext();
-  useEffect(() => {
-    async function getTokens() {
-      let web3 = new Web3(provider);
-      const contract = new web3.eth.Contract(
-        ABI,
-        "0xAF7FF053dF6a38F004DCfB964fAE4Bef6f479E6a"
-      );
-      const cc = await contract.methods.name().call();
-      console.log(cc);
-    }
-    getTokens()
-      .then(() => console.log("Read"))
-      .catch((err) => console.log("Not Read", err));
-  }, []);
+  // useEffect(() => {
+  //   async function getTokens() {
+  //     let web3 = new Web3(provider);
+  //     const contract = new web3.eth.Contract(
+  //       ABI,
+  //       "0xAF7FF053dF6a38F004DCfB964fAE4Bef6f479E6a"
+  //     );
+  //     const cc = await contract.methods.totalSupply().call();
+  //     console.log(cc);
+  //   }
+  //   getTokens()
+  //     .then(() => console.log("Read"))
+  //     .catch((err) => console.log("Not Read", err));
+  // }, []);
+
   return (
     <div className="">
       <Hero />
--- providers/ethProvider.tsx
+++ providers/ethProvider.tsx
@@ -12,23 +12,14 @@ export const detectProvider = () => {
   return provider;
 };
 
-export async function readFromSmartContract() {
-  const currentProvider = detectProvider();
-  let web3: Web3;
-  web3 = new Web3(currentProvider);
-  const contract = new web3.eth.Contract(
-    ABI,
-    "0xAF7FF053dF6a38F004DCfB964fAE4Bef6f479E6a"
-  );
-  const cc = await contract.methods.name().call();
-  console.log(cc);
-}
-// let tokenName, tokenSymbol, tokenTotalSupply;
-//   tokenName = await contract.methods.name().call();
-//   console.log("Name: ", tokenName);
-
-//   tokenSymbol = await contract.methods.symbol().call();
-//   console.log("Symbol: ", tokenSymbol);
-
-//   tokenTotalSupply = await contract.methods.totalSupply().call();
-//   console.log("Total supply: ", tokenTotalSupply);
+// export async function readFromSmartContract() {
+//   const currentProvider = detectProvider();
+//   let web3: Web3;
+//   web3 = new Web3(currentProvider);
+//   const contract = new web3.eth.Contract(
+//     ABI,
+//     "0xAF7FF053dF6a38F004DCfB964fAE4Bef6f479E6a"
+//   );
+//   const cc = await contract.methods.name().call();
+//   console.log(cc);
+// }
