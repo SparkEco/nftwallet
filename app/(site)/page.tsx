56c9d72139dbf9fdd434c94904000280e450fa97
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-10-27 11:56:27 +0100

    Added: loading screen to routes

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,19 +1,31 @@
+"use client";
+
 import Amaze from "@/components/Amaze";
 import Collections from "@/components/Collections";
 import Create from "@/components/Create";
 import Hero from "@/components/Hero";
+import IsLoading from "@/components/Loading";
 import TopCollection from "@/components/TopCollection";
+import { useAppContext } from "@/context/AppContext";
 
 function Home() {
+  const { allData } = useAppContext();
+
   return (
-    <div className="">
-      <Hero />
-      <Create />
-      <Collections />
-      <TopCollection />
-      <Amaze />
-      {/* <Discover /> */}
-    </div>
+    <>
+      {allData.length === 0 ? (
+        <IsLoading />
+      ) : (
+        <div className="">
+          <Hero />
+          <Create />
+          <Collections />
+          <TopCollection />
+          <Amaze />
+          {/* <Discover /> */}
+        </div>
+      )}
+    </>
   );
 }
 
