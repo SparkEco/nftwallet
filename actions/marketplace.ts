7d1440cf96afc8e5a9dd4d7072760f9bc4254027
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-11-05 13:13:51 +0100

    Fixing purchase

--- actions/marketplace.ts
+++ actions/marketplace.ts
@@ -2,6 +2,8 @@
 
 import ABI from "@/ABIs/marketplaceAbi.json";
 import { createContract } from "./serverActions";
+import { Contract } from "ethers";
+import { getProvider } from "./clientActions";
 const contractAddress = "0xB594Cdeb1b46254A11Fc69d25D1a726aEbf9642c";
 
 export async function getAllListing() {
@@ -19,7 +21,9 @@ export async function getAllListing() {
 
 export async function purchaseListing(amount: any, index: number) {
   try {
-    const contract = await createContract(contractAddress, ABI);
+    const { provider } = await getProvider();
+    const signer = await provider?.getSigner();
+    const contract = new Contract(contractAddress, ABI, signer);
     await contract?.purchaseListing(amount, index);
   } catch (err) {
     console.error("Purchase Failed:", err);
