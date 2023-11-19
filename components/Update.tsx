6b55d7ce2808c4abc76cbf661d8cd0d3b9c3eefe
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-11-19 09:03:38 +0100

    Fixed modal close error

--- components/Update.tsx
+++ components/Update.tsx
@@ -59,10 +59,7 @@ function Update({ children, data }: UpdateProps) {
       <AlertDialog.Trigger asChild>{children}</AlertDialog.Trigger>
       <AlertDialog.Portal>
         <AlertDialog.Overlay className="fixed bg-neutral-900/90 inset-0 backdrop-blur z-[21]" />
-        <AlertDialog.Content
-          onClick={(e) => e.stopPropagation()}
-          className="fixed focus:outline-none drop-shadow-md border space-y-3 z-[22] border-neutral-700 top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]  h-[95vh] lg:w-[45vw] w-[85vw] rounded-md bg-white p-[25px]"
-        >
+        <AlertDialog.Content className="fixed focus:outline-none drop-shadow-md border space-y-3 z-[22] border-neutral-700 top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]  h-[95vh] lg:w-[45vw] w-[85vw] rounded-md bg-white p-[25px]">
           <AlertDialog.Title
             className={`text-center text-[22px] font-semibold`}
           >
