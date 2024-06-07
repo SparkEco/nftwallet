96851d4cc8bafc166eba34cdce912555fcc1537e
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-06-07 07:48:56 +0100

    Added select for country

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -3,35 +3,15 @@
 import "react-datetime-picker/dist/DateTimePicker.css";
 import "react-calendar/dist/Calendar.css";
 import "react-clock/dist/Clock.css";
-import { Label } from "@/components/ui/label";
-import Image from "next/image";
-import Form2 from "@/components/Form2";
 import Form3 from "@/components/Form3";
+import Form2 from "@/components/Form2";
 
 export interface AllImageData {
   image: string;
 }
 
 type ValuePiece = Date | null;
-
 export type Value = ValuePiece | [ValuePiece, ValuePiece];
-
-export const Field = ({
-  label,
-  children,
-}: {
-  label: string;
-  children: React.ReactNode;
-}) => {
-  return (
-    <fieldset className={`w-full`}>
-      <Label className={`font-semibold`}>
-        {label}
-        {children}
-      </Label>
-    </fieldset>
-  );
-};
 
 function Page() {
   return <Form3 />;
