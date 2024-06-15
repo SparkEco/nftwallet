0c1e58c17701e5131b90ae20a77d864453f21c05
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-06-15 15:32:34 +0100

    Adapted create collection page to mobile

--- components/CountrySelect.tsx
+++ components/CountrySelect.tsx
@@ -44,7 +44,7 @@ const CountrySelect = React.memo(({ setState, value }: CountrySelectProps) => {
         >
           <path d="M8.47 1.97a.75.75 0 0 1 1.06 0l4.897 4.896a1.25 1.25 0 0 1 0 1.768L9.53 13.53a.75.75 0 0 1-1.06-1.06l3.97-3.97H1.75a.75.75 0 1 1 0-1.5h10.69L8.47 3.03a.75.75 0 0 1 0-1.06"></path>
         </svg>
-        <span>Select recipient's country</span>
+        <span>Select recipient&apos;s country</span>
       </Label>
       <Select
         name="location_country"
