dde0845f2690b42ce815ac89cb5f1bf7ceb40dce
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-06-28 02:29:15 +0100

    Added arweave as filestorage

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    serverComponentsExternalPackages: ["arweave"],
+    serverComponentsExternalPackages: ["arweave", "@irys/sdk"],
   },
 };
 
